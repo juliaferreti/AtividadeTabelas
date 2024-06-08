a1bfdd65b69272ccb63facddb1674db258b73b43
Júlia Ferreti <113567445+juliaferreti@users.noreply.github.com>
2024-06-08 17:35:08 -0300

    ModeloDAO.php

--- src/Models/DAO/ModeloDAO.php
+++ src/Models/DAO/ModeloDAO.php
@@ -24,5 +24,51 @@ class ModeloDAO{
             return 0;
         }
     }
-}
 
+    public function alterar(Modelo $modelo){
+        try{
+            $sql= "UPDATE modelo SET descricao = :descricao, tamanho = :tamanho, marca = :marca WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":descricao", $modelo->getDescricao());
+            $p->bindValue(":tamanho", $modelo->getTamanho());
+            $p->bindValue(":marca", $modelo->getMarca());
+            $p->bindValue(":id", $modelo->getId());
+            return $p->execute();
+
+        }catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function excluir($id){
+        try{
+            $sql = "DELETE FROM modelo WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":id", $id);
+            return $p->execute();
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function consultarTodos(){
+        try{
+            $sql = "SELECT * FROM modelo";
+            return $this->conexao->getConexao()->query($sql);
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function consultar($id){
+        try{
+            $sql = "SELECT * FROM modelo WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":id", $id);
+            $p->execute();
+            return $p->fetch();
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+}
