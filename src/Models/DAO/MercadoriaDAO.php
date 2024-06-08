3d06e9215047b988f41e2c8d3f9fde7fb795724b
Júlia Ferreti <113567445+juliaferreti@users.noreply.github.com>
2024-06-08 17:34:37 -0300

    MercadoriaDAO.php

--- src/Models/DAO/MercadoriaDAO.php
+++ src/Models/DAO/MercadoriaDAO.php
@@ -24,5 +24,51 @@ class MercadoriaDAO{
             return 0;
         }
     }
-}
 
+    public function alterar(Mercadoria $mercadoria){
+        try{
+            $sql= "UPDATE mercadoria SET nome = :nome, numerodeserie = :numerodeserie, validade = :validade WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":nome", $mercadoria->getNome());
+            $p->bindValue(":numerodeserie", $mercadoria->getNumerodeserie());
+            $p->bindValue(":validade", $mercadoria->getValidade());
+            $p->bindValue(":id", $mercadoria->getId());
+            return $p->execute();
+
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function excluir($id){
+        try{
+            $sql = "DELETE FROM mercadoria WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":id", $id);
+            return $p->execute();
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function consultarTodos(){
+        try{
+            $sql = "SELECT * FROM mercadoria";
+            return $this->conexao->getConexao()->query($sql);
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function consultar($id){
+        try{
+            $sql = "SELECT * FROM mercadoria WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":id", $id);
+            $p->execute();
+            return $p->fetch();
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+}
