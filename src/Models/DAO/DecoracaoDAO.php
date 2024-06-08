e08aed642c70269f192193ba578ab7c31d3204a8
Júlia Ferreti <113567445+juliaferreti@users.noreply.github.com>
2024-06-08 17:33:55 -0300

    DecoracaoDAO.php

--- src/Models/DAO/DecoracaoDAO.php
+++ src/Models/DAO/DecoracaoDAO.php
@@ -24,5 +24,51 @@ class DecoracaoDAO{
             return 0;
         }
     }
-}
 
+    public function alterar(Decoracao $decoracao){
+        try{
+            $sql= "UPDATE decoracao SET descricao = :descricao, cobertura = :cobertura, fruta = :fruta WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":descricao", $decoracao->getDescricao());
+            $p->bindValue(":cobertura", $decoracao->getCobertura());
+            $p->bindValue(":fruta", $decoracao->getFruta());
+            $p->bindValue(":id", $decoracao->getId());
+            return $p->execute();
+
+        }catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function excluir($id){
+        try{
+            $sql = "DELETE FROM decoracao WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":id", $id);
+            return $p->execute();
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function consultarTodos(){
+        try{
+            $sql = "SELECT * FROM decoracao";
+            return $this->conexao->getConexao()->query($sql);
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function consultar($id){
+        try{
+            $sql = "SELECT * FROM decoracao WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":id", $id);
+            $p->execute();
+            return $p->fetch();
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+}
