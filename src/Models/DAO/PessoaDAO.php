593b15e3a4fcfe68c630deed8abd3d2fcc179a43
Júlia Ferreti <113567445+juliaferreti@users.noreply.github.com>
2024-06-08 17:35:51 -0300

    PessoaDAO.php

--- src/Models/DAO/PessoaDAO.php
+++ src/Models/DAO/PessoaDAO.php
@@ -24,5 +24,51 @@ class PessoaDAO{
             return 0;
         }
     }
-}
 
+    public function alterar(Pessoa $pessoa){
+        try{
+            $sql= "UPDATE pessoa SET nome = :nome, cpf = :cpf, telefone = :telefone WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":nome", $pessoa->getNome());
+            $p->bindValue(":cpf", $pessoa->getCpf());
+            $p->bindValue(":telefone", $pessoa->getTelefone());
+            $p->bindValue(":id", $pessoa->getId());
+            return $p->execute();
+
+        }catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function excluir($id){
+        try{
+            $sql = "DELETE FROM pessoa WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":id", $id);
+            return $p->execute();
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function consultarTodos(){
+        try{
+            $sql = "SELECT * FROM pessoa";
+            return $this->conexao->getConexao()->query($sql);
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function consultar($id){
+        try{
+            $sql = "SELECT * FROM pessoa WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":id", $id);
+            $p->execute();
+            return $p->fetch();
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+}
